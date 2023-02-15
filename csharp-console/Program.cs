72b73f2d6433e8479007065df2edc1aecfe0b1ef
Julien Le Mee <julemee@gmail.com>
2023-02-15 15:52:31 -0500

    added options menu

--- csharp-console/Program.cs
+++ csharp-console/Program.cs
@@ -1,3 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System
 
+puclic class cardHolder
+{
+    String cardNum;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardName, int pin, string firstName, string lsatName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public String getNum()
+    {
+        return cardNum;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public static void main(String[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+    }
+
+}
